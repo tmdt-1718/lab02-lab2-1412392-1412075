9f0bf12c7c32561306d6dc8f15fb97393e106bb3
1412392 <nguyenthanhphi0401@gmail.com>
2017-10-26 02:11:46 +0700

    ok

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module Lab21412392
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 	config.assets.initialize_on_precompile = false
+	config.serve_static_assets = true
+	config.assets.compile = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
